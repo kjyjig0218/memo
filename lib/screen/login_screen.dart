a5adcc746794e2fd7bbd044a2977bff341ea40b3
jeong-yujin <kjyjig0218@naver.com>
2023-12-05 00:00:21 +0900

    마지막 저장!!

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -41,18 +41,32 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            OutlinedButton(
-                onPressed: () async{
+            Image.asset("images/bee.png"),
+            Text("My Memo",
+            style: TextStyle(fontSize: 40,)),
+            SizedBox(height: 40),
 
-                  GoogleSignInAccount? _account = await _googleSignIn.signIn();
-                  print(_account?.email);
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("images/google icon.png",height: 30,width: 30,),
+                SizedBox(width: 5),
+                OutlinedButton(
+
+                    onPressed: () async{
+
+                      GoogleSignInAccount? _account = await _googleSignIn.signIn();
+                      print(_account?.email);
 
 
-                  //todo 경우 1 : 기존유저 없음 -> 회원가입 요청후 가입된 회원정보 가져오기 /
-                  //todo 경우 2 : 이미 가입된 회원이라면 -> 회원가입 없이 회원정보 가져오기
-                },
-                child: Text('Google Login')
-            )
+                      //todo 경우 1 : 기존유저 없음 -> 회원가입 요청후 가입된 회원정보 가져오기
+                      //todo 경우 2 : 이미 가입된 회원이라면 -> 회원가입 없이 회원정보 가져오기
+                    },
+                    child: Text('Google Login')
+                )
+              ],
+            ),
+
 
           ],
         ),
